f1685e7bddfef8b820986d47c160aea7367e7cdc
zhdltl601 <zhdltl601@gmail.com>
2024-07-04 19:08:04 +0900

    Fixed Enemies

--- Assets/_Scr/Jung/Enemy/Common/Common/EnemyRunAwayState.cs
+++ Assets/_Scr/Jung/Enemy/Common/Common/EnemyRunAwayState.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class EnemyRunAwayState : EnemyState
 {
-    private Vector3 runAwayTargetPos;
+    private Vector3 targetPos;
     
     public EnemyRunAwayState(Enemy enemy, Animator animator, string animBoolName) : base(enemy, animator, animBoolName)
     {
@@ -15,36 +15,32 @@ public class EnemyRunAwayState : EnemyState
     public override void Enter()
     {
         base.Enter();
-        
+
         _enemy.NavMeshAgent.isStopped = false;
         _enemy.runningAway = true;
-        
-        runAwayTargetPos = _enemy.runAwayTrm.position;
 
-        _enemy.NavMeshAgent.SetDestination(runAwayTargetPos);
+        targetPos = _enemy.runAwayTrm.position;
     }
 
     public override void Update()
     {
         base.Update();
-        Debug.DrawRay(_enemy.transform.position + new Vector3(0,1,0), -(_enemy.transform.position - runAwayTargetPos).normalized * 3, Color.green, Time.deltaTime);
-        if (Vector3.Distance(_enemy.transform.position, runAwayTargetPos) <= 1.5f || 
-            Physics.Raycast(_enemy.transform.position, -(_enemy.transform.position - runAwayTargetPos).normalized, 3f))
+
+        if (Vector3.Distance(_enemy.transform.position, targetPos) <= 0.4f ||
+            Physics.Raycast(_enemy.transform.position, _enemy.target.forward, 2f) == true)
         {
-           _enemy.StateMachine.ChangeState(_enemy.IdleState);
+            _enemy.StateMachine.ChangeState(_enemy.IdleState);
         }
-        //else
-        //{
-        //    _enemy.NavMeshAgent.SetDestination(runAwayTargetPos);
-        //}
+
+        _enemy.NavMeshAgent.SetDestination(targetPos);
     }
 
     public override void Exit()
     {
         base.Exit();
 
-        //_enemy.transform.rotation = Quaternion.LookRotation(_enemy.target.position);
-        
+        _enemy.transform.rotation = Quaternion.LookRotation(_enemy.target.position);
+
         _enemy.NavMeshAgent.isStopped = true;
         _enemy.runningAway = false;
         _enemy.runAwayCount--;
